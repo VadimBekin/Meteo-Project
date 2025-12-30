5873a30369de820c0f4d36349e680b3ffe46fdec
VadimBekin <vadimbeckin@yandex.by>
2025-12-30 17:49:50 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Meteo-Project',
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: '/Meteo-Project',
+
       },
     }),
   ],
